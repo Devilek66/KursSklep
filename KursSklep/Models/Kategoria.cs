4a4f66d0ae812e9b1e40f24b1f881a258df953ef
Devilek66 <Aleksanderp1993@gmail.com>
2017-11-15 04:35:01 +0100

    dodanie atrybutów do klas naszego modelu

--- KursSklep/Models/Kategoria.cs
+++ KursSklep/Models/Kategoria.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace KursSklep.Models
 {
     public class Kategoria
     {
         public int KategoriaId { get; set; }
+        [Required(ErrorMessage = "Wprowadź nazwę kategorii")]
+        [StringLength(100)]
         public string NazwaKategorii { get; set; }
+        [Required(ErrorMessage = "Wprowadź opis kategorii")]
         public string OpisKategorii { get; set; }
         public string NazwaPlikuIkony { get; set; }
 
